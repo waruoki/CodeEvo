ab8322a3eb3efd55176d3d7661bcf86722b7f57f
waruoki <redwankabir28@gmail.com>
2026-09-16 22:46:15 +0600

    Creating webpage_generator.py for HTML generation
    Just another side project. Its fun

---
+++ webpage_generator.py
@@ -0,0 +1,15 @@
+page_title = "py webpage lab"
+heading_page = "Welcome to the page"
+paragraph_text = "This webpage was generated automatically using python code"
+
+html_blueprint = f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{page_title}</title>
+</head>    
+<body>
+    <h1>{heading_page}</h1>
+    <p>{paragraph_text}</p>
+</body>
+</html>"""
